bbcb913a1de4e77064085a3edd561e2278376b8c
Abhishek singh <abhisheksingh19217@gmail.com>
2025-11-24 01:45:23 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://jobportalbackend-kpg7.onrender.com/api' });
 
 export function setAuthToken(token) {
   if (token) API.defaults.headers.common['Authorization'] = `Bearer ${token}`;
